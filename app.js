659d196bb662c019e22c859c65b5f95771a782a4
ZM-Project2204 <mridul.vishram@gmail.com>
2026-06-17 01:52:12 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,32 @@
 function login(){
 
 let username=document.getElementById("user").value.trim();
-
 let password=document.getElementById("pass").value.trim();
 
 if(username==="admin" && password==="zm@2026"){
 
 window.location.href="dashboard.html";
+return;
 
-}else{
+}
 
-document.getElementById("error").innerHTML="Invalid Username or Password";
+let users=JSON.parse(localStorage.getItem("users"))||[];
+
+let found=users.find(
+u=>u.username===username && u.password===password
+);
+
+if(found){
+
+localStorage.setItem("currentUser",found.username);
+
+window.location.href="user.html";
+
+}
+else{
+
+document.getElementById("error").innerHTML=
+"Invalid Username or Password";
 
 }
 
