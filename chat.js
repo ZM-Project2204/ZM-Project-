6c133f01fe178f08d319f23c29f6320c23c21253
ZM-Project2204 <mridul.vishram@gmail.com>
2026-06-17 16:01:42 +0530

    Update chat.js

--- chat.js
+++ chat.js
@@ -1,23 +1,39 @@
-function send(){
+let chat = JSON.parse(localStorage.getItem("chat")) || [];
 
-let msg=document.getElementById("message").value;
+function render() {
+
+let box = document.getElementById("chatbox");
+
+box.innerHTML = "";
+
+chat.forEach(m => {
+
+box.innerHTML += `
+<div class="msg me">
+${m}
+</div>
+`;
+
+});
+
+box.scrollTop = box.scrollHeight;
+
+}
+
+function send() {
+
+let msg = document.getElementById("message").value.trim();
 
 if(msg=="") return;
 
-document.getElementById("chatbox").innerHTML+=`
+chat.push(msg);
 
-<div class="msg me">
-
-${msg}
-
-</div>
-
-`;
+localStorage.setItem("chat",JSON.stringify(chat));
 
 document.getElementById("message").value="";
 
-let box=document.getElementById("chatbox");
-
-box.scrollTop=box.scrollHeight;
+render();
 
 }
+
+render();
